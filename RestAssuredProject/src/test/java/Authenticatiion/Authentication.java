5a4b62d6ec5899ba4fc126f09790150ec13532eb
Krishnat-Hogale_frgr <Krishnat.Hogale@nttdata.com>
2024-07-31 11:07:22 +0000

    Authentication and typre

--- RestAssuredProject/src/test/java/Authenticatiion/Authentication.java
+++ RestAssuredProject/src/test/java/Authenticatiion/Authentication.java
@@ -33,11 +33,49 @@ public class Authentication {
     @Test(priority = 3)
     void testPremitiveAuthentication(){
         given()
-                .auth().basic("postman","password")
+                .auth().preemptive().basic("postman","password")
                 .when()
                 .get("https://postman-echo.com/basic-auth").then()
                 .statusCode(200)
                 .body("authenticated",equalTo(true))
+                .log().all();
+    }
+    @Test
+    void testBearerTokenAuthentication(){
+
+        String beareeToken = "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";
+        given()
+                .headers("Authorization","Bearer "+beareeToken).when()
+                .get("https://github.com/nttd-KrishnatHogale?tab=repositories").then()
+                .statusCode(200).log().all();
+
+    }
+
+    @Test
+    void testOAuth1Authentication(){
+        given()
+                .auth().oauth("consumerKey","consumerSecret","accessToken","tokenSecrete")
+                .when()
+                .get("url")
+                .then().statusCode(200).log().all();
+
+    }
+
+    @Test
+    void testAuth2Authentication(){
+        given()
+                .auth().oauth2("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")
+                .when()
+                .get("https://github.com/nttd-KrishnatHogale?tab=repositories").then()
+                .statusCode(200).log().all();
+    }
+
+    @Test
+    void testAPIKAYAuthentication(){
+        given()
+                .queryParam("","").when()
+                .get("url").then()
+                .statusCode(200)
                 .log().all();
     }
 }
